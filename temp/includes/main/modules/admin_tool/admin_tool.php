0c53fe5a5712dbb97f4e3537d51f924b18b8c0bb
jwdavis1981 <jwdavis1981@gmail.com>
2012-12-10 23:10:40 -0500

    Removing old versions code from repo.

--- temp/includes/main/modules/admin_tool/admin_tool.php
+++
@@ -1,5 +0,0 @@
-<?php // echo 'admin tool dashboard';
-if (!session_id()) session_start();
-echo '';
-require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/main/modules/admin_tool/client/clientadmin.php");
-?>
--- temp/includes/main/modules/admin_tool/permission/permadmin.php
+++
@@ -1,1 +0,0 @@
-<?php echo 'Permission Administration coming soon!'; ?>
