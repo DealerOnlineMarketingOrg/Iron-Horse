0a11e51bf477f51be06bf2c4e5b3344ebae0ae1f
Jeremy Davis <jwdavis@dealeronlinemarketing.com>
2012-12-13 15:51:56 -0500

    New navigation

--- DomCMS/views/Global/incl/nav.php
+++ DomCMS/views/Global/incl/nav.php
@@ -2,12 +2,16 @@
     <div class="wrapper">
         <ul class="main_nav relative">
             <li class="dashboard">
-            	<a href="<?= base_url(); ?>"><span>Dashboard</span></a>
+            	<a href="<?= base_url(); ?>"><span>Reports</span></a>
                 <div class="subnav" id="dashboard_nav">
                     <div class="pivot">&nbsp;</div>
-                    <a href="#">Dashboard</a>
+                    <a href="<?= base_url(); ?>dashboard">Dashboard</a>
                     <a href="#">DPR</a>
-                    <a href="#">Reports</a>
+                    <a href="#">Reports Query</a>
+                    <a href="#">Game Day</a>
+                    <a href="#">DMS Database</a>
+                    <a href="#">Web</a>
+                    <a href="#">Leads</a>
                     <div class="clear"></div>
                 </div>
             </li>
@@ -15,20 +19,23 @@
             	<a href="<?= base_url(); ?>advertising"><span>Advertising</span></a>
                 <div class="subnav" id="advertising_nav">
                     <div class="pivot">&nbsp;</div>
-                    <a href="#">E-Blasts</a>
-                    <a href="#">Newsletters</a>
-                    <a href="#">CRM Templates</a>
+                    <a href="#" class="google">Google</a>
+                    <a href="#" class="yahoo">Yahoo</a>
+                    <a href="#" class="bing">Bing</a>
+                    <a href="#" class="facebook">Facebook</a>
                     <div class="clear"></div>
                 </div>
             </li>
-	<li class="creative">
+			<li class="creative">
             	<a href="<?= base_url(); ?>creative"><span>Creative</span></a>
                 <div class="subnav" id="creative_nav">
                     <div class="pivot">&nbsp;</div>
-                    <a href="#">Assets</a>
-                    <a href="#">Logos</a>
-                    <a href="#">Videos</a>
-                    <a href="#">Audio</a>
+                    <a href="#">Campaigns</a>
+                    <a href="#">E-Blasts</a>
+                    <a href="#">Newsletters</a>
+                    <a href="#">CRM Templates</a>
+                    <a href="#">Specials</a>
+                    <a href="#">Misc</a>
                     <div class="clear"></div>
                 </div>
             </li>
@@ -36,11 +43,11 @@
                 <a href="<?= base_url(); ?>content"><span>Content</span></a>
                 <div class="subnav" id="content_nav">
                     <div class="pivot">&nbsp;</div>
-                    <a href="#">Content Pages</a>
+                    <a href="#">Dealership Pages</a>
                     <a href="#">New Cars</a>
                     <a href="#">Used Cars</a>
-                    <a href="#">Specials</a>
-                    <a href="#">Archive</a>
+                    <a href="#">Variable Ops Pages</a>
+                    <a href="#">Fixed Ops Pages</a>
                     <div class="clear"></div>
                 </div>
             </li>
@@ -48,32 +55,22 @@
             	<a href="<?= base_url(); ?>merchandising"><span>Merchandising</span></a>
                 <div class="subnav" id="merch_nav">
                     <div class="pivot">&nbsp;</div>
-                    <a href="#">New Car Specials</a>
-                    <a href="#">Used Car Specials</a>
-                    <a href="#">Service Specials</a>
-                    <a href="#">Parts Specials</a>
-                    <a href="#">Body Shop Specials</a>
-                    <div class="clear"></div>
-                </div>
-            </li>
-            <li class="dms">
-            	<a href="<?= base_url(); ?>dms"><span>DMS Database</span></a>
-                <div class="subnav" id="dms_nav">
-                    <div class="pivot">&nbsp;</div>
-                    <a href="#">DMS</a>
-                    <a href="#">Custom Report</a>
+                    <a href="#">New Car</a>
+                    <a href="#">Used Car</a>
+                    <a href="#">Service</a>
+                    <a href="#">Parts</a>
+                    <a href="#">Body Shop</a>
                     <div class="clear"></div>
                 </div>
             </li>
             <li class="reputation">
-            	<a href="<?= base_url(); ?>reputation"><span>Reputation</span></a>
+            	<a href="<?= base_url(); ?>reputation"><span>Reputation/Social</span></a>
                 <div class="subnav" id="rep_nav">
                     <div class="pivot">&nbsp;</div>
-                    <a href="#">Content Pages</a>
-                    <a href="#">New Cars</a>
-                    <a href="#">Used Cars</a>
-                    <a href="#">Specials</a>
-                    <a href="#">Archive</a>
+                    <a href="#" class="google_plus">Google+</a>
+                    <a href="#" class="yelp">Yelp</a>
+                    <a href="#" class="yahoo">Yahoo</a>
+                    <a href="#" class="bing">Bing</a>
                     <div class="clear"></div>
                 </div>
             </li>
@@ -81,13 +78,14 @@
             	<a href="<?= base_url(); ?>admin/"><span>Admin</span></a>
                 <div class="subnav" id="admin_nav">
                     <div class="pivot">&nbsp;</div>
+                    <a href="#">Portal</a>
                     <a href="<?= base_url(); ?>admin/agency">Agency</a>
                     <a href="#">Groups</a>
                     <a href="#">Clients</a>
                     <a href="#">Users</a>
                     <a href="#">Contacts</a>
                     <a href="#">Websites</a>
-                    <a href="#">Preferences</a>
+                    <a href="#">Master List</a>
                     <a href="#">My Account</a>
                     <div class="clear"></div>
                 </div>
