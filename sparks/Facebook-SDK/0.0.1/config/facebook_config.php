fba94815b8a4448d85c4d5705e667ae090db92ee
Jeremy Davis <jwdavis@dealeronlinemarketing.com>
2012-12-20 16:07:59 -0500

    Removing sparks packages

--- sparks/Facebook-SDK/0.0.1/config/facebook_config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$config['uri_protocol']    = "PATH_INFO";
-$config['permitted_uri_chars'] = 'a-z 0-9?~%.:_\-';
-$config['enable_query_strings'] = TRUE;
-
-$config['appId'] = '[your appID]';
-$config['secret'] = '[your secret]';
